d583472db080a6bc1ee3b49e72878b14272acf54
ilhammursidi <imursidi37@gmail.com>
2026-04-27 14:14:15 +0700

    feat: add minitask2

--- main.go
+++ main.go
@@ -6,8 +6,16 @@ import (
 )
 
 func main() {
+	// minitask 1
 	fmt.Println(luas(5))
 	fmt.Println(keliling(5))
+
+	fmt.Println(luasDanKeliling(5))
+
+	var n int
+	fmt.Println("Input angka : ")
+	fmt.Scan(&n)
+	triangle(n)
 }
 
 func luas(r float64) float64 {
@@ -16,4 +24,21 @@ func luas(r float64) float64 {
 
 func keliling(r float64) float64 {
 	return 2 * math.Pi * r
+}
+
+func luasDanKeliling(r float64) (luas float64, keliling float64) {
+	luas = math.Pi * r * r
+	keliling = 2 * math.Pi * r
+	return luas, keliling
+}
+
+func triangle(n int) int {
+
+	for i := 0; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+	return n
 }
