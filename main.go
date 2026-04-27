96323a08e491e3c800db1ec1d4fab83f8e547759
ilhammursidi <imursidi37@gmail.com>
2026-04-27 16:41:35 +0700

    feat: add minitask4

--- main.go
+++ main.go
@@ -12,10 +12,56 @@ func main() {
 
 	fmt.Println(luasDanKeliling(5))
 
+	// minitask 2
 	var n int
-	fmt.Println("Input angka : ")
+	fmt.Print("Input angka : ")
 	fmt.Scan(&n)
 	triangle(n)
+
+	// minitask 3
+	slice := []int{50, 75, 66, 20, 32, 90}
+	var newSlice []int = slice[:3]
+	var endSlice []int = slice[3:]
+
+	var allSlice []int = make([]int, 0, 7)
+	allSlice = append(allSlice, newSlice...)
+	allSlice = append(allSlice, 88)
+	allSlice = append(allSlice, endSlice...)
+	fmt.Println(slice)
+	fmt.Println(newSlice)
+	fmt.Println(endSlice)
+	fmt.Println(allSlice)
+
+	// minitask 4
+	ilham := user{
+		// id:        1,
+		name:      "Ilham Mursidi",
+		image:     "",
+		email:     "imursidi37@gmail.com",
+		umur:      26,
+		phone:     "082189971471",
+		isMarried: false,
+		education: education{name: "SMKN 1 Minasatene", major: "Teknik Komputer dan Jaringan"},
+	}
+
+	fmt.Println(ilham.education)
+	fmt.Println(ilham)
+}
+
+type user struct {
+	// id        int
+	name      string
+	image     string
+	email     string
+	umur      int
+	phone     string
+	isMarried bool
+	education education
+}
+
+type education struct {
+	name  string
+	major string
 }
 
 func luas(r float64) float64 {
