b4357391c5cbeb64bdca7526a6e19895161bd250
ilhammursidi <imursidi37@gmail.com>
2026-04-27 11:48:41 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ilhammursidi/koda-b7-go
+
+go 1.26.0
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(luas(math.Pi, 5))
+}
+
+func luas(pi float64, r float64) float64 {
+	return pi * r * r
+}
+
+func keliling()
